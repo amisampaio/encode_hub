457e9990d70c0145d1b117633498a28a831f874c
Amilcar <amilcar.sampaio@gmail.com>
2021-02-19 14:55:49 -0300

    1.0

---
+++ README.rst
@@ -0,0 +1,11 @@
+cypto_hub for Python
+===================
+
+Installation:
+
+    pip install cypto_hub
+
+Usage
+
+    from cypto_hub import cypto_hub
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='cypto_hub',
+    version='1.0.0',
+    packages=['cypto_hub'],
+    url='https://github.com/amisampaio/cypto_hub.git',
+    license='Apache 2.0',
+    author='Amilcar Sampaio',
+    author_email='amilcar.sampaio@gmail.com',
+    description='cypto_hub'
+)
